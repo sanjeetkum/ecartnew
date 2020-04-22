5d34bc3ce673e540c42eb9cab9609e89306902a6
Kumar, Sanjeet <sanjekumar@deloitte.com>
2020-04-22 17:18:04 +0530

    Updates

---
+++ precache-manifest.a574178337bd681d51b11df50a1c6e69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038268751ed4d520695181827c7c68f6",
+    "url": "/sanjeetkum/shoppingcart/index.html"
+  },
+  {
+    "revision": "079db4d2ed56c3478f87",
+    "url": "/sanjeetkum/shoppingcart/static/css/main.7bfcca87.chunk.css"
+  },
+  {
+    "revision": "20ef08c0e19c54db0fb2",
+    "url": "/sanjeetkum/shoppingcart/static/js/2.ee81b71e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/sanjeetkum/shoppingcart/static/js/2.ee81b71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079db4d2ed56c3478f87",
+    "url": "/sanjeetkum/shoppingcart/static/js/main.eea0429e.chunk.js"
+  },
+  {
+    "revision": "cc7f8b98216bc98f1cae",
+    "url": "/sanjeetkum/shoppingcart/static/js/runtime-main.c6067d97.js"
+  },
+  {
+    "revision": "65444c77f5c3938f10be557e97addfa0",
+    "url": "/sanjeetkum/shoppingcart/static/media/bare-tree.65444c77.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sanjeetkum/shoppingcart/static/media/logo.5d5d9eef.svg"
+  }
+]);
