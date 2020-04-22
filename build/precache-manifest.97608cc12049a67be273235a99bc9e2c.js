fe9cca52e501a9e20945f9a990a100572d5d3e56
Kumar, Sanjeet <sanjekumar@deloitte.com>
2020-04-22 18:37:09 +0530

    added

---
+++ build/precache-manifest.97608cc12049a67be273235a99bc9e2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157e11190e751b39823ee3b838e2e46c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "151ca81a8fd4dcb6c71c",
+    "url": "./static/css/main.7bfcca87.chunk.css"
+  },
+  {
+    "revision": "af28b1cfa1fc7eb1ac14",
+    "url": "./static/js/2.4368be0c.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "./static/js/2.4368be0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151ca81a8fd4dcb6c71c",
+    "url": "./static/js/main.b2587cef.chunk.js"
+  },
+  {
+    "revision": "deaa65e90d69f0d44559",
+    "url": "./static/js/runtime-main.aa4b3c70.js"
+  },
+  {
+    "revision": "65444c77f5c3938f10be557e97addfa0",
+    "url": "./static/media/bare-tree.65444c77.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
